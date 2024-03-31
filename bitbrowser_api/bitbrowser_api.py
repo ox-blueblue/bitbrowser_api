af14ad1bdbc4d892383e9ea393a97e16d87869fd
embzheng <embzheng@qq.com>
2024-03-31 14:58:00 +0800

    1、修复bug

--- bitbrowser_api/bitbrowser_api.py
+++ bitbrowser_api/bitbrowser_api.py
@@ -2,14 +2,15 @@ from playwright.sync_api import Playwright, sync_playwright, expect, Page, Eleme
 import requests
 import json
 import random
+from my_logtool import log
 import wallet_tool
-from logger import log
+
 # 官方文档地址
-# httpsdoc2.bitbrowser.cnjiekouben-di-fu-wu-zhi-nan.html
+# https://doc2.bitbrowser.cn/jiekou/ben-di-fu-wu-zhi-nan.html
 
 # 新建一个窗口
-class BitBrowser
-    def __init__(self, url, headers, id, proxy_type='', proxy_host='', proxy_port='', proxy_user='', proxy_pwd='')
+class BitBrowser:
+    def __init__(self, url, headers, id, proxy_type='', proxy_host='', proxy_port='', proxy_user='', proxy_pwd=''):
         self.url = url
         self.headers = headers
         self.id = id        
@@ -22,137 +23,150 @@ class BitBrowser
         self.proxy_pwd = proxy_pwd
         self.proxy_ip = '' #需要chack agent 后获取
         # 新窗口参数
-        self.finger_print =   
-        self.update_proxy()    
+        self.detail = None
+        if proxy_type != '':
+            self.update_proxy() 
 
-    def open(self)
-        json_data = {id f'{self.id}'}
-        res = requests.post(f{self.url}browseropen,
+    def open(self):
+        json_data = {"id": f'{self.id}'}
+        res = requests.post(f"{self.url}/browser/open",
                             data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
+        if res["success"]:            
             ws = res['data']['ws']
-            log.info(fopen browser ws address == {ws})
+            log.info(f"open browser ws address ==>>> {ws}")
             self.ws = ws
-        else
-            log.error(res[msg])
-        return self.ws
-    
-    def close(self)
-        json_data = {'id' f'{self.id}'}
-        res = requests.post(f{self.url}browserclose,
-                    data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
-            log.info(fclose browser success{self.id})
+            ret = self.get_detail()
+            if ret == False:
+                return False
             return True
-        else
-            log.error(res[msg])
+        else:
+            log.error(res["msg"])
+        return False
+    
+    def close(self):
+        json_data = {'id': f'{self.id}'}
+        res = requests.post(f"{self.url}/browser/close",
+                    data=json.dumps(json_data), headers=self.headers).json()
+        if res["success"]:            
+            log.info(f"close browser success:{self.id}")
+            return True
+        else:
+            log.error(res["msg"])
         return False
         
-    def update(self, valuedict)
-        json_data = {'id' f'{id}'}
+    def update(self, value:dict):
+        json_data = {'id': f'{id}'}
         json_data.update(value)
-        res = requests.post(f{self.url}browserupdatepartial,
+        res = requests.post(f"{self.url}/browser/update/partial",
                             data=json.dumps(json_data), headers=self.headers).json()
         return res
     
-    #  'remark' '我是一个备注', 'browserFingerPrint' {}}
-    def update_remark(self, remark)
-        json_data = {'id' f'{id}'}
-        json_data.update({'remark'remark})
-        res = requests.post(f{self.url}browserupdatepartial,
+    #  'remark': '我是一个备注', 'browserFingerPrint': {}}
+    def update_remark(self, remark):
+        json_data = {'id': f'{id}'}
+        json_data.update({'remark':remark})
+        res = requests.post(f"{self.url}/browser/update/partial",
                             data=json.dumps(json_data), headers=self.headers).json()
         return res
             
-    def update_finger_print(self, finger_print='')
-        if finger_print == ''
+    def update_finger_print(self, finger_print=''):
+        if finger_print == '':
             finger_print = self.radom_finger_pring()
-        json_data = {ids [self.id,]}
-        json_data.update({'browserFingerPrint'finger_print})
-        res = requests.post(f{self.url}browserupdatepartial,
+        json_data = {"ids": [self.id,]}
+        json_data.update({'browserFingerPrint':finger_print})
+        res = requests.post(f"{self.url}/browser/update/partial",
                             data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
-            log.info(fupdate finger success{self.id})
+        if res["success"]:            
+            log.info(f"update finger success:{self.id}")
             return True
-        else
-            log.error(res[msg])
+        else:
+            log.error(res["msg"])
         return False
     
-    def radom_finger_pring(self)
-        # browserfingerprintrandom
-        json_data = {'browserId' f'{self.id}'}
-        res = requests.post(f{self.url}browserfingerprintrandom,
+    def radom_finger_pring(self):
+        # /browser/fingerprint/random
+        json_data = {'browserId': f'{self.id}'}
+        res = requests.post(f"{self.url}/browser/fingerprint/random",
                             data=json.dumps(json_data), headers=self.headers).json()
-        self.finger_print = res[data]
-        return self.finger_print
+        finger_print = res["data"]
+        return finger_print
     
-    def update_proxy(self)
-        json_data = {ids [self.id,],         #string
-            proxyMethod 2,        # 代理方式 2自定义 3 提取IP            
-            'proxyType' self.proxy_type, # 代理类型  ['noproxy', 'http', 'https', 'socks5', 'ssh']
-            'host' self.proxy_host,  # 代理主机
-            'port' self.proxy_port,  # 代理端口
-            'proxyUserName' self.proxy_user,  # 代理账号
-            'proxyPassword' self.proxy_pwd
+    def update_proxy(self):
+        json_data = {"ids": [self.id,],         #string
+            "proxyMethod": 2,        # 代理方式 2自定义 3 提取IP            
+            'proxyType': self.proxy_type, # 代理类型  ['noproxy', 'http', 'https', 'socks5', 'ssh']
+            'host': self.proxy_host,  # 代理主机
+            'port': self.proxy_port,  # 代理端口
+            'proxyUserName': self.proxy_user,  # 代理账号
+            'proxyPassword': self.proxy_pwd
             }
-        res = requests.post(f{self.url}browserproxyupdate,
+        res = requests.post(f"{self.url}/browser/proxy/update",
                         data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
-            log.info(fupdate proxy success{self.proxy_user})
+        if res["success"]:            
+            log.info(f"update proxy success:{self.proxy_user}")
             return True
-        else
-            log.error(res[msg])
+        else:
+            log.error(res["msg"])
         return False
     
     # 删除全部缓存，包括云端数据
-    def cache_clear(self)
-        json_data = {ids [self.id,]}
-        res = requests.post(f{self.url}cacheclear,
+    def cache_clear(self):
+        json_data = {"ids": [self.id,]}
+        res = requests.post(f"{self.url}/cache/clear",
                         data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
-            log.info(fcache clear success{self.id})
+        if res["success"]:            
+            log.info(f"cache clear success:{self.id}")
             return True
-        else
-            log.error(res[msg])
+        else:
+            log.error(res["msg"])
         return False
         
-    def cache_clear_except_ex(self)
-        json_data = {ids [self.id,]}
-        res = requests.post(f{self.url}cacheclearexceptExtensions,
+    def cache_clear_except_ex(self):
+        json_data = {"ids": [self.id,]}
+        res = requests.post(f"{self.url}/cache/clear/exceptExtensions",
                         data=json.dumps(json_data), headers=self.headers).json()
-        if res[success]            
-            log.info(fcache clear exceptExtensions success{self.id})
+        if res["success"]:            
+            log.info(f"cache clear exceptExtensions success:{self.id}")
             return True
-        else
-            log.error(res[msg])
+        else:
+            log.error(res["msg"])
         return False
     
-    def check_agent(self)        
-        json_data = {host self.proxy_host,         #string
-                 port self.proxy_port,        #int 
-                 proxyType self.proxy_type,   #string
-                 proxyUserName self.proxy_user, #string
-                 proxyPassword self.proxy_pwd, #string
-                 checkExists 1, 
+    def check_agent(self):        
+        json_data = {"host": self.proxy_host,         #string
+                 "port": self.proxy_port,        #int 
+                 "proxyType": self.proxy_type,   #string
+                 "proxyUserName": self.proxy_user, #string
+                 "proxyPassword": self.proxy_pwd, #string
+                 "checkExists": 1, 
                  }
-        res = requests.post(f{self.url}checkagent,
+        res = requests.post(f"{self.url}/checkagent",
                         data=json.dumps(json_data), headers=self.headers).json()   
-        if res[data][success]
-            self.proxy_ip = res[data][data][ip] 
-            log.info(fcheck agent success{self.proxy_ip})
+        if res["data"]["success"]:
+            self.proxy_ip = res["data"]["data"]["ip"] 
+            log.info(f"check agent success:{self.proxy_ip}")
             return True
-        else
-            log.error(res[data][message])
-        return False    
+        else:
+            log.error(res["data"]["message"])
+        return False   
+
+    def get_detail(self):
+        json_data = {"id": self.id}
+        res = requests.post(f"{self.url}/browser/detail",
+                        data=json.dumps(json_data), headers=self.headers).json()   
+        if res["success"]:
+            self.detail = res["data"] 
+            log.debug(self.detail)
+            finger_print = self.detail["browserFingerPrint"]["userAgent"]
+            log.info(f"get detail success:FingerPrint:{finger_print}")
+            return True
+        else:
+            log.error(res["error"])
+        return False
        
 
-if __name__ == '__main__':       
-    browser_id = 3547827ebf3a4253aa01f6a0e13ba95e    
-    b = BitBrowser(
-        url=http127.0.0.154345,
-        headers={'Content-Type' 'applicationjson'},
-        id=browser_id
-    )        
-    b.update_finger_print()
-    b.cache_clear()        
-    b.open()
-            
+# if __name__ == '__main__':
+#     #browser_id = createBrowser()
+#     # listBrowser()
+#     # pidsOfBrowser()
+    
--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
+project_urls = {
+  'Source': 'https://github.com/embzheng/bitbrowser_api'
+}
 setuptools.setup(
     name="bitbrowser_api",
-    version="0.1",
+    version="0.3",
     author="embzheng",
     author_email="embzheng@qq.com",
     description="This is a bitbrowser operation tool",
@@ -13,10 +15,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/embzheng/bitbrowser_api",
     packages=setuptools.find_packages(),
-    install_requires=['playwright>=1.42.0','requests','wallet_tool'],    
+    install_requires=['playwright>=1.42.0','requests','wallet_tool','my_logtool'],    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    project_urls = project_urls
 )
